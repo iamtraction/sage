ebdac170f84fe6ff63ea4a37ba7502e64695b260
TRACTION <19631364+iamtraction@users.noreply.github.com>
2026-02-18 03:23:53 +0530

    feat: validate git repository before running CLI
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- cmd/git-sage/main.go
+++ cmd/git-sage/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+
+	"git-sage/internal/git"
 )
 
 func main() {
+	ctx := context.Background()
+	inside, err := git.IsGitRepo(ctx)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "git-sage: %v\n", err)
+		os.Exit(1)
+	}
+	if !inside {
+		fmt.Fprintln(os.Stderr, "git-sage: not a git repository")
+		os.Exit(1)
+	}
 	fmt.Println("git-sage: run 'git sage help' for more information")
 }

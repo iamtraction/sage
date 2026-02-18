d9d8633798d49054d7e2bc4bd783487108309b2d
TRACTION <19631364+iamtraction@users.noreply.github.com>
2026-02-19 02:30:54 +0530

    feat: use new command handler
    Signed-off-by: TRACTION <19631364+iamtraction@users.noreply.github.com>

--- cmd/git-sage/main.go
+++ cmd/git-sage/main.go
@@ -1,47 +1,11 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"os"
 
-	"git-sage/internal/git"
-	"git-sage/internal/tokens"
+	"git-sage/internal/cmd"
 )
 
-const tokenLimit = 100_000
-
 func main() {
-	ctx := context.Background()
-	inside, err := git.IsGitRepo(ctx)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "git-sage: %v\n", err)
-		os.Exit(1)
-	}
-	if !inside {
-		fmt.Fprintln(os.Stderr, "git-sage: not a git repository")
-		os.Exit(1)
-	}
-	hasStaged, err := git.HasStagedChanges(ctx)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "git-sage: %v\n", err)
-		os.Exit(1)
-	}
-	if !hasStaged {
-		fmt.Fprintln(os.Stderr, "git-sage: no staged changes")
-		os.Exit(0)
-	}
-	diff, err := git.GetStagedDiff(ctx, 3)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "git-sage: %v\n", err)
-		os.Exit(1)
-	}
-	if tokens.Estimate(diff) > tokenLimit {
-		diff, err = git.GetStagedDiff(ctx, 0)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "git-sage: %v\n", err)
-			os.Exit(1)
-		}
-	}
-	fmt.Printf("%d tokens\n", tokens.Estimate(diff))
+	os.Exit(cmd.Run(os.Args[1:]))
 }
